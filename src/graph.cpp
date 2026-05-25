54c90fa7d5fda1fba15e58935088d0c999a525b1
Bhhrian <hy2onbeen@gmail.com>
2026-05-25 00:06:03 -0700

    Disjoint clas Constructor

--- src/graph.cpp
+++ src/graph.cpp
@@ -3,3 +3,11 @@
 void error(string msg){
     cerr << "Error: " << msg << endl;
 }
+
+//Disjoint class
+DisjointSet::DisjointSet(int numVertices){
+    subsets.resize(numVertices);
+    for (Vertex i = 0; i < numVertices; ++i) {
+        makeSet(i);
+    }
+}
